d42dd82d9754010c782ea1d3f9685947d1191dca
Austtiin <austinstephens103@gmail.com>
2024-03-13 23:43:54 -0500

    Create BOLReport.java

---
+++ SpotlessStockX/src/mainApp/BOLReport.java
@@ -0,0 +1,14 @@
+package mainApp;
+
+public class BOLReport {
+
+	public BOLReport() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
